34505efd8e79305adbebfe6277b7a7d9dafb98c5
Nguyễn Gia Qúy <quytun10032003@gmail.com>
2025-11-23 23:05:26 +0700

    Add layout and home

---
+++ App/src/app/containers/client/home/home.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { Gallery } from '../../../core/model/gallery';
+import { Product } from '../../../core/model/product';
+import { Article } from '../../../core/model/article';
+import { Menu } from '../../../core/model/menu';
+import { MenuService } from '../../../core/service/menu.service';
+import { ArticleService } from '../../../core/service/article.service';
+import { GalleryService } from '../../../core/service/gallery.service';
+import { ProductService } from '../../../core/service/product.service';
+import { ShareModule } from '../../../share.module';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [ShareModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  mainBanners: Gallery[] = [];
+  productSellings: Product[] = [];
+  menuHomePages: Menu[] = [];
+  highlightArticle: Article[] = [];
+
+  customOptions: OwlOptions = {
+    loop: false,
+    items: 2,
+    margin: 10,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    nav: false
+  }
+
+  constructor(
+    public menuService: MenuService,
+    public articleService: ArticleService,
+    public galleryService: GalleryService,
+    public productService: ProductService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getBanner();
+    this.getProductSelling();
+    this.getAllMenuHomePage();
+    this.getHighlightArticle();
+  }
+
+  getBanner() {
+    this.galleryService.get({})
+      .subscribe((resp: any) => {
+        let datas: Gallery[] = resp;
+        this.mainBanners = datas.filter(x => x.type == 1);
+      })
+  }
+
+  getProductSelling() {
+    this.productService.getSelling()
+      .subscribe((resp: any) => {
+        this.productSellings = resp;
+      })
+  }
+  updateLocalWishlist(updated: Product) {
+  // 1. Cập nhật productSellings
+  this.productSellings = this.productSellings.map(p =>
+    p.id === updated.id ? { ...p, isWishlist: updated.isWishlist } : p
+  );
+
+  // 2. Cập nhật menuHomePages
+  this.menuHomePages = this.menuHomePages.map(menu => ({
+    ...menu,
+    products: menu.products.map(p =>
+      p.id === updated.id ? { ...p, isWishlist: updated.isWishlist } : p
+    )
+  }));
+}
+onWishlistToggled() {
+  // Nếu muốn refresh cả hai:
+  this.getProductSelling();
+  this.getAllMenuHomePage();
+}
+  getAllMenuHomePage() {
+    this.menuService.getAllMenuHomePage()
+      .subscribe((resp: any) => {
+        this.menuHomePages = resp;
+      })
+  }
+
+  getHighlightArticle() {
+    this.articleService.getHighlight()
+      .subscribe((resp: any) => {
+        this.highlightArticle = resp;
+      })
+  }
+}
